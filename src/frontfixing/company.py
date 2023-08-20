995b43537c0b2ec199f5d19bd639a89384627f84
Alvin De la Cruz <alvinj.delacruz@gmail.com>
2023-08-20 12:59:24 +0100

    Refactoring

--- src/frontfixing/company.py
+++ src/frontfixing/company.py
@@ -1,22 +1,122 @@
+from abc import ABC, abstractmethod
 import datetime
 
 import numpy as np
 
-from scipy import interpolate
 from scipy.sparse import diags
-from scipy.optimize import root
 
-from option import Option
+from option import Option, OptionType
 from utils import solve
+
+class ExplicitSolver(ABC):
+    def __init__(
+        self,
+        r: float,  # risk-free interest rate
+        sigma: float,  # sigma price volatitliy
+        dx: float,  # grid resolution along x-axis
+        dt: float,  # grid resolution along t-axis
+        delta  # dividends
+    ): 
+        self.r = r
+        self.sigma = sigma
+        self.dx = dx
+        self.dt = dt
+        self.delta = delta    
+        self.lambd = self.dt / np.power(self.dx,2)
+
+        self.A = 0.5*self.lambd*np.power(self.sigma, 2) - 0.5*self.lambd*((self.r-self.delta)-np.power(self.sigma,2))*self.dx
+        self.B = 1 - np.power(self.sigma, 2) * self.lambd - self.r*self.dt
+        self.C = 0.5*self.lambd*np.power(self.sigma, 2) + 0.5*self.lambd*((self.r-self.delta)-np.power(self.sigma,2))*self.dx
+
+        self.alpha = 1 + self.r*np.power(self.dx/self.sigma, 2)
+        self.beta = 1 + self.dx + 0.5*np.power(self.dx, 2)
+
+    @abstractmethod
+    def compute_time_iteration(self, p: np.ndarray, S_bar: float) -> float:
+        pass
+
+    def solve(self, option: Option):
+            """Solve front-fixing method explicitly.
+
+            Parameters:
+                option (AmericanOption): the option to price.
+                r (float): the stock price risk-free interest rate.
+                sigma (float): the stock price volatility.
+                x_max (float): large value used as infity in the spatial.
+                dx (float): Grid resolution in the spatial direction.
+                dt (float): Grid resolution in the time direction.
+            """
+
+            p = np.zeros_like(self.x_axis)
+            S_bar = 1
+            for _ in np.arange(0, option.T, self.dt):
+                S_bar = self.compute_time_iteration(p, S_bar)
+
+            S = S_bar * np.exp(self.x_axis)
+            return S, p, S_bar
+    
+class CallExplicitSolver(ExplicitSolver):
+    def __init__(
+        self,
+        r: float,  # risk-free interest rate
+        sigma: float,  # sigma price volatitliy
+        x_min: float,  # sufficiently large value for x
+        dx: float,  # grid resolution along x-axis
+        dt: float,  # grid resolution along t-axis
+        delta=0  # dividends
+    ):                 
+        self.x_axis = np.arange(x_min-dx, 0, dx)
+        super().__init__(r=r, sigma=sigma, dx=dx, dt=dt, delta=delta)
+
+    def compute_time_iteration(self, p: np.ndarray, S_bar: float) -> float:
+        d = self.alpha + (self.A*p[-3] + self.B*p[-2] + self.C*p[-1] - (p[-1]-p[-3])/(2*self.dx))
+        d /= (p[-1]-p[-3])/(2*self.dx) + self.beta*S_bar
+        
+        S_bar_new = d*S_bar
+        a = self.A - (S_bar_new - S_bar)/(2*self.dx*S_bar)
+        c = self.C + (S_bar_new- S_bar)/(2*self.dx*S_bar)
+
+        S_bar = S_bar_new
+        p[1:-2] = a*p[:-3] + self.B*p[1:-2] + c*p[2:-1]
+        p[-2] = self.beta*S_bar - self.alpha 
+        p[-1] = S_bar - 1
+        return S_bar_new
+
+class PutExplicitSolver(ExplicitSolver):
+    def __init__(
+        self,
+        r: float,  # risk-free interest rate
+        sigma: float,  # sigma price volatitliy
+        x_max: float,  # sufficiently large value for x
+        dx: float,  # grid resolution along x-axis
+        dt: float,  # grid resolution along t-axis
+        delta=0  # dividends
+    ):                 
+        self.x_axis = np.arange(0, x_max+dx, dx)
+        super().__init__(r=r, sigma=sigma, dx=dx, dt=dt, delta=delta)
+
+    def compute_time_iteration(self, p: np.ndarray, S_bar: float) -> float:
+        d = self.alpha - (self.A*p[0] + self.B*p[1] + self.C*p[2] - (p[2]-p[0])/(2*self.dx))
+        d /= (p[2]-p[0])/(2*self.dx) + self.beta*S_bar
+        
+        S_bar_new = d*S_bar
+        a = self.A - (S_bar_new - S_bar)/(2*self.dx*S_bar)
+        c = self.C + (S_bar_new- S_bar)/(2*self.dx*S_bar)
+
+        S_bar = S_bar_new
+        p[2:-1] = a*p[1:-2] + self.B*p[2:-1] + c*p[3:]
+        p[0] = 1 - S_bar
+        p[1] = self.alpha - self.beta*S_bar
+        return S_bar_new
 
 def solve_explicitly(
     option: Option,  # the option
     r: float,  # risk-free interest rate
     sigma: float,  # sigma price volatitliy
-    x_max: float,  # sufficiently large value for x
     dx: float,  # grid resolution along x-axis
     dt: float,  # grid resolution along t-axis
-    delta=0  # dividends
+    delta=0,  # dividends
+    **kwargs,
 ):
     """Solve front-fixing method explicitly.
 
@@ -25,36 +125,16 @@ def solve_explicitly(
         r (float): the stock price risk-free interest rate.
         sigma (float): the stock price volatility.
         x_max (float): large value used as infity in the spatial.
-        dx (float): Grid resolution in the spatial direction.
-        dt (float): Grid resolution in the time direction.
+        N (float): Grid resolution in the spatial direction.
+        M (float): Grid resolution in the time direction.
     """
-    lambd = dt / np.power(dx,2)
-    x = np.arange(0, x_max+dx, dx)
-
-    A = 0.5*lambd*np.power(sigma, 2) - 0.5*lambd*((r-delta)-np.power(sigma,2))*dx
-    B = 1 - np.power(sigma, 2) * lambd - r*dt
-    C = 0.5*lambd*np.power(sigma, 2) + 0.5*lambd*((r-delta)-np.power(sigma,2))*dx
-
-    alpha = 1 + r*np.power(dx/sigma, 2)
-    beta = 1 + dx + 0.5*np.power(dx, 2)
-    
-    p = np.zeros_like(x)
-    S_bar = 1
-    for _ in np.arange(0, option.T, dt):
-        d = alpha - (A*p[0] + B*p[1] + C*p[2] - (p[2]-p[0])/(2*dx))
-        d /= (p[2]-p[0])/(2*dx) + beta*S_bar
-        
-        S_bar_new = d*S_bar
-        a = A - (S_bar_new - S_bar)/(2*dx*S_bar)
-        c = C + (S_bar_new- S_bar)/(2*dx*S_bar)
-
-        S_bar = S_bar_new
-        p[2:-1] = a*p[1:-2] + B*p[2:-1] + c*p[3:]
-        p[0] = 1 - S_bar
-        p[1] = alpha - beta*S_bar
-
-    S = S_bar * np.exp(x)
-    return S, p, S_bar
+    match option.type:
+        case OptionType.CALL:
+            x_min = kwargs.get('x_min', -2)
+            return CallExplicitSolver(r, sigma, x_min, dx, dt, delta).solve(option)
+        case OptionType.PUT:
+            x_max = kwargs.get('x_max', 2)
+            return CallExplicitSolver(r, sigma, x_max, dx, dt, delta).solve(option)
 
 def solve_implicitly(
     option: Option,  # the option
